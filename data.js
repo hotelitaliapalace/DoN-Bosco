78add909b14400d567a3b9701a3322aecb93cd9a
pehlivan2022 <99028696+pehlivan2022@users.noreply.github.com>
2025-06-09 04:07:44 +0200

    DoN_v___t___

---
+++ data.js
@@ -0,0 +1,566 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-messale-superiore",
+      "name": "Messale superiore",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": -3.09191789795611,
+        "pitch": 0.12594370739340377,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.072773939395603,
+          "pitch": 0.032446892802951766,
+          "rotation": 0,
+          "target": "2-entrata-e-parco"
+        },
+        {
+          "yaw": 0.09579825484920867,
+          "pitch": 0.13568925179793112,
+          "rotation": 3.141592653589793,
+          "target": "1-lettura"
+        },
+        {
+          "yaw": -1.2328055665538677,
+          "pitch": 0.24684158227875663,
+          "rotation": 0,
+          "target": "4-navata-sinistra"
+        },
+        {
+          "yaw": 1.3658286368872812,
+          "pitch": 0.26513694314072467,
+          "rotation": 0,
+          "target": "4-navata-sinistra"
+        },
+        {
+          "yaw": -3.074124309080391,
+          "pitch": 0.14372388542413717,
+          "rotation": 3.141592653589793,
+          "target": "8-navata-cripta"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-lettura",
+      "name": "Lettura",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 2.653850649946108,
+        "pitch": 0.5924998252209832,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1745043145175327,
+          "pitch": 0.2534453986557139,
+          "rotation": 0,
+          "target": "3-navata-destra"
+        },
+        {
+          "yaw": -2.0576319710191804,
+          "pitch": 0.2031319413508328,
+          "rotation": 0,
+          "target": "4-navata-sinistra"
+        },
+        {
+          "yaw": 2.6411136273835325,
+          "pitch": 0.25767488009665485,
+          "rotation": 3.141592653589793,
+          "target": "0-messale-superiore"
+        },
+        {
+          "yaw": 2.6400393970006446,
+          "pitch": 0.09281658505102364,
+          "rotation": 0,
+          "target": "2-entrata-e-parco"
+        },
+        {
+          "yaw": 2.643046533933993,
+          "pitch": -0.4580103194952656,
+          "rotation": 0,
+          "target": "9-lignano-sabbiadoro"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-entrata-e-parco",
+      "name": "Entrata e parco",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -3.1372410033531484,
+        "pitch": -0.07707002989745604,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1392190225499306,
+          "pitch": -0.16124950255745674,
+          "rotation": 0,
+          "target": "5-altare-superiore"
+        },
+        {
+          "yaw": 3.1393117410940166,
+          "pitch": 0.01812579087685151,
+          "rotation": 3.141592653589793,
+          "target": "7-altare-cripta"
+        },
+        {
+          "yaw": 3.13194507142902,
+          "pitch": -0.7600194823259656,
+          "rotation": 0,
+          "target": "9-lignano-sabbiadoro"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-navata-destra",
+      "name": "Navata destra",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-navata-sinistra",
+      "name": "Navata sinistra",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 0.3325947276231531,
+        "pitch": 0.10324860308134376,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.395836708030238,
+          "pitch": 0.04152074904438052,
+          "rotation": 0,
+          "target": "5-altare-superiore"
+        },
+        {
+          "yaw": -3.0356975764801923,
+          "pitch": -0.15104027352955285,
+          "rotation": 3.141592653589793,
+          "target": "0-messale-superiore"
+        },
+        {
+          "yaw": 2.8704679182714585,
+          "pitch": -0.04540467100108003,
+          "rotation": 0,
+          "target": "1-lettura"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-altare-superiore",
+      "name": "Altare superiore",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0739393702354825,
+          "pitch": 0.16675191642184295,
+          "rotation": 0,
+          "target": "3-navata-destra"
+        },
+        {
+          "yaw": -1.0828329757760162,
+          "pitch": 0.14854195349598953,
+          "rotation": 0,
+          "target": "4-navata-sinistra"
+        },
+        {
+          "yaw": 3.132548826811872,
+          "pitch": -0.5444821749500441,
+          "rotation": 0,
+          "target": "9-lignano-sabbiadoro"
+        },
+        {
+          "yaw": -3.13212692817066,
+          "pitch": 0.10141447926518055,
+          "rotation": 3.141592653589793,
+          "target": "7-altare-cripta"
+        },
+        {
+          "yaw": 0.035910685859294134,
+          "pitch": -0.06773014139224642,
+          "rotation": 3.141592653589793,
+          "target": "1-lettura"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-altare-superiore-con-le-navate",
+      "name": "Altare superiore con le navate",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-altare-cripta",
+      "name": "Altare cripta",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.4930230439532224,
+        "pitch": 0.3839068609454195,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4686797453102134,
+          "pitch": 0.1505907120990564,
+          "rotation": 3.141592653589793,
+          "target": "8-navata-cripta"
+        },
+        {
+          "yaw": 0.47261930125708496,
+          "pitch": 0.023985498673422256,
+          "rotation": 0,
+          "target": "2-entrata-e-parco"
+        },
+        {
+          "yaw": 0.4732787402745817,
+          "pitch": -0.3461603959411459,
+          "rotation": 0,
+          "target": "1-lettura"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-navata-cripta",
+      "name": "Navata cripta",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.10904058043787401,
+        "pitch": 0.09398784133836102,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0619012440272098,
+          "pitch": -0.17152550455929472,
+          "rotation": 0,
+          "target": "1-lettura"
+        },
+        {
+          "yaw": 0.06243970016354439,
+          "pitch": 0.05395199035129217,
+          "rotation": 3.141592653589793,
+          "target": "7-altare-cripta"
+        },
+        {
+          "yaw": -3.0563263853675373,
+          "pitch": -0.0033313973989166357,
+          "rotation": 3.141592653589793,
+          "target": "2-entrata-e-parco"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-lignano-sabbiadoro",
+      "name": "Lignano Sabbiadoro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.6357497241688677,
+        "pitch": 0.46601025361560744,
+        "fov": 0.6683723575017722
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6345460217239562,
+          "pitch": 0.4637678537806913,
+          "rotation": 3.141592653589793,
+          "target": "2-entrata-e-parco"
+        },
+        {
+          "yaw": -0.6753167395021098,
+          "pitch": 0.07137032339311133,
+          "rotation": 0,
+          "target": "10-lignano-riviera"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6344218782097926,
+          "pitch": 0.4107036047817978,
+          "title": "Don Giovani Bosco Lignano Sabbiadoro<div><br></div>",
+          "text": "Text"
+        },
+        {
+          "yaw": -3.0576176364052667,
+          "pitch": 1.5442518379009043,
+          "title": "Hotel Italia PAlace",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "10-lignano-riviera",
+      "name": "Lignano Riviera",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.1903183602636087,
+        "pitch": 0.5034728114201403,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5224596081571082,
+          "pitch": 0.17229564611699644,
+          "rotation": 0,
+          "target": "2-entrata-e-parco"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Don Giovani Bosco Lignano Sabbiadoro",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
